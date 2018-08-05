0559ab8ee1aed4fc5e3746dac0d0a46ed8986e6f
Rezillien <kaziorradoslaw@gmail.com>
2018-08-05 19:46:13 +0200

    somehow it's working

--- src/mirko.js
+++ src/mirko.js
@@ -1,20 +1,28 @@
 
+window.bottomFlag=true;
+window.shouldWork=false;
 $(document).ready(()=>{
   window.bottomFlag=false;
+  window.pageCounter=2;
+  window.appendAfter = '.comments-stream ';
+  window.appendTemplate = 'wtykopHotAppendClass';
+  if($('div.nav.bspace.rbl-block > ul:nth-child(3) > li:nth-child(3)')[0].className==='active'){
+    window.shouldWork=true;
+  }
 });
 
+async function asyncAppendList(){
+  $(window.appendAfter).after(
+    $('<div class="'+ window.appendTemplate + window.pageCounter +'"></div>').load('https://www.wykop.pl/mikroblog/hot/strona/' + window.pageCounter + '/ .comments-stream'));
+  window.appendAfter = '.' + window.appendTemplate + window.pageCounter;
+  window.pageCounter++;
+}
+
 $(window).scroll(function() {
-  if(!window.bottomFlag && $(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+  if(!window.bottomFlag && $(window).scrollTop() + $(window).height() > $(document).height() - 500) {
     window.bottomFlag = true;
     setTimeout(()=>{window.bottomFlag=false;},1000);
-    let items=$('.comments-stream');
-    $.get('https://www.wykop.pl/mikroblog/hot/strona/2/').done((secondPageItems)=>{
-      console.log(items);
-      let secondPage = $.parseHTML(secondPageItems);
-
-      window.seconPage = secondPage;
-
-    });
+    asyncAppendList();
 
   }
 });
