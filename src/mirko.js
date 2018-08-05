2d5e10c242314be70a88c8e116200f53a3f2ab6f
Rezillien <kaziorradoslaw@gmail.com>
2018-08-05 17:37:54 +0200

    initial wtykop commit

---
+++ src/mirko.js
@@ -0,0 +1,19 @@
+
+$(document).ready(()=>{
+  window.bottomFlag=false;
+});
+
+$(window).scroll(function() {
+  if(!window.bottomFlag && $(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+    window.bottomFlag = true;
+    setTimeout(()=>{window.bottomFlag=false;},1000);
+    let items=$('.comments-stream');
+    $.get('https://www.wykop.pl/mikroblog/hot/strona/2/').done((secondPageItems)=>{
+      console.log(items);
+      console.log($.parseHTML(secondPageItems));
+      window.seconPageItems = secondPageItems;
+
+    });
+
+  }
+});
